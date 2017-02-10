699dcadfa5ce080bce28b43261d88c6edbd31353
pautorres <entimos34@gmail.com>
2017-02-10 12:11:24 +0100

    I added two functions. The second one is used in the code of the first one. I’ll explain you later

---
+++ R/SNIPS.R
@@ -0,0 +1,34 @@
+SNIPS <- function(indnames, SNIPgroups, g1, g2, batchmat){
+        
+        #This two lines transform the rownames of the matrix selecting the second
+        #string, which match with the labels from indnames.
+        splitnames <- strsplit(rownames(SNIPgroups),"-")
+        samenames <- sapply(splitnames, function(x)  x[[2]])
+        #Select the names of the matrix that appears in indnames
+        #The out of intersect follows the same order of indnames
+        common <- intersect(indnames, samenames)
+        
+        #Returns the position of each member in common in the SNIP matrix files
+        selsubs <- sapply(common, function(x) match(x,samenames))
+        
+        #Select only the files of the matrix that are in common, putting the files in the same order
+        #as in common
+        genosCommon <- SNIPgroups[selsubs,]
+        
+        #A vector of the length of indnames telling if each individual
+        #appears on the matrix.
+        groupCommon <- as.factor(as.numeric(indnames%in%common))
+        
+        #Now apply getDatagroup to sepparate the distances between individuals
+        #of which we have the genotype from the others.
+        #The output of each list will have $1 and $0 as names. The useful ones are
+        # in $1
+        g1new <- getDataGroup(x = g1, group = groupCommon)
+        g2new <- getDataGroup(x = g2, group = groupCommon)
+        batchmatnew <- getDataGroup(x= batchmat, group = groupCommon )
+        
+        ans <- lapply(X = 1:ncol(genosCommon), function(x) trymcGroup(g1new[[2]], g2new[[2]], batchmatnew[[2]], genosCommon[,x]))
+        
+        names(ans) <- colnames(genosCommon)
+        ans
+}
---
+++ R/trymcGroup.R
@@ -0,0 +1,23 @@
+trymcGroup <- function(g1, g2, batchmat, group){
+        
+        if(any(is.na(group))==T){
+                NAS <- factor(as.numeric((complete.cases(group))))
+                G1 <- getDataGroup(x = g1, group = NAS)
+                G2 <- getDataGroup(x = g2, group = NAS)
+                B <- getDataGroup(x= batchmat, group = NAS)
+                
+                g1 <- G1[[2]]
+                g2 <- G2[[2]]
+                batchmat <- B[[2]]
+                group <- group[complete.cases(group)]
+        }
+        group <- as.factor(group)
+        levels(group) <-c("AA", "AB", "BB")
+        
+        test <- try(mantelcorGroup(g1, g2, batchmat, as.factor(group)),silent = T)
+        if(class(test)=="try-error")
+        {
+                test<-NA
+        }
+        test
+}
