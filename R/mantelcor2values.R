8d6d649579a138ca85c2db4009d1922ce1a06b88
pautorres <entimos34@gmail.com>
2017-05-31 12:01:16 +0200

    He añadido la función mantelcor2values

--- R/mantelcor2values.R
+++ R/mantelcor2values.R
@@ -1,6 +1,4 @@
-#Levels of groups are A & B.(A=AA +AB, B=BB)
-
-mantelcor2Group  <- function(g1, g2, batchmat, group) {
+mantelcor2values <- function(g1, g2, batchmat, group){
         if (!is.factor(group)){stop("group variable must be a factor")}
         
         list1 <- getDataGroup(x = g1 ,group = group)
@@ -10,8 +8,8 @@ mantelcor2Group  <- function(g1, g2, batchmat, group) {
         MA <- try(mantelcor(list1$A, list2$A, list3$A, use = "complete.obs"))
         MB <- try(mantelcor(list1$B, list2$B, list3$B, use = "complete.obs"))
         
-        if(class(MA)=="try-error" || class(MB)=="try-error"){
-                return(NA)
-        }
-        abs(MA-MB)
+        if(class(MA)=="try-error"){ MA <- NA }
+        if(class(MB)=="try-error"){ MB <- NA }
+        
+        return(c(MA, MB))
 }
