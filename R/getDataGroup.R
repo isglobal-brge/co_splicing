01191586a4baab724975fccc161fdebef566b1da
pautorres <entimos34@gmail.com>
2017-06-27 12:38:07 +0200

    He eliminado dos archivos innecesarios y he comentado una función

--- R/getDataGroup.R
+++ R/getDataGroup.R
@@ -1,3 +1,16 @@
+#'  getDataGroup
+#' 
+#' This function separate the distance between individuals data contained in x accordingly with 
+#' his class on the variable group.
+#' 
+#' @param x:  A vector with the distance between individuals data. The vector contains the lower
+#' triangle of a distance matrix filled in the same order as the lower.tri() function.
+#' @param group: A factor with the group to which each individual of the distance matrix belong.
+#' The order of individuals on this variable must be the same as the distance matrix. 
+#' @return It returns a list with number of elements equal to the number of levels on group,
+#' i.e, the number of classes. Each element contains a vector with the distances between individuals
+#' belonging to the same group. The elements are stuck on the same order as they were on vector x.
+#' @export 
 
 getDataGroup <- function(x, group) {
   if (!is.factor(group))
